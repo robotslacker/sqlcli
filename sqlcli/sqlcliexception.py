fa27f73a84483a2b9d460a7f1bbbd3f185fabbaa
robotslacker <184902652@qq.com>
2021-01-11 02:37:48 +0800

    支持exception的直接__str__

--- sqlcli/sqlcliexception.py
+++ sqlcli/sqlcliexception.py
@@ -5,3 +5,6 @@ class SQLCliException(Exception):
     def __init__(self, message):
         Exception.__init__(self)
         self.message = message
+
+    def __str__(self):
+        return self.message
