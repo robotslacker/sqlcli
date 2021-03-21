2472a10a8d8c3ddbe525a09ea3cf82f1d2315f75
robotslacker <184902652@qq.com>
2021-03-22 02:03:36 +0800

    增加remoteserver，重构目前的程序

--- sqlcli/sqlcliremoteserver.py
+++ sqlcli/sqlcliremoteserver.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import random
