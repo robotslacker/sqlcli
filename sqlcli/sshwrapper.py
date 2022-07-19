b712f04ad09835489574b88a02d0465a602a51f7
robotslacker <184902652@qq.com>
2022-07-19 13:48:47 +0800

    JQParse支持对整形的处理

--- sqlcli/sshwrapper.py
+++ sqlcli/sshwrapper.py
@@ -14,6 +14,10 @@ class SshWrapper(object):
         self.sftp = None
         self.ssh = None
         self.__transport__ = None
+        self.shell = None
+        self.shell_stdin = None
+        self.shell_stdout = None
+        self.invokeShell = False
 
     def sshConnectWithPassword(self, pHostName, pUserName, pPassWord):
         self.host = pHostName
@@ -33,20 +37,85 @@ class SshWrapper(object):
         print("UserName=" + pUserName + " keyfile=" + pKeyFile)
         pass
 
+    def sshSetInvokeShell(self, invokeShell):
+        if not self.invokeShell and invokeShell:
+            self.invokeShell = True
+            self.shell = self.ssh.invoke_shell()
+            self.shell_stdin = self.shell.makefile('wb')
+            self.shell_stdout = self.shell.makefile('r')
+            self.shell_stdout.channel.set_combine_stderr(True)
+            self.sshExecuteShellCommand("export PS1=$")
+
+    def sshExecuteShellCommand(self, pCommand):
+        cmd = pCommand.strip('\n')
+        self.shell_stdin.write(cmd + '\n')
+        finish = 'end of stdOUT buffer. finished with exit status'
+        echo_cmd = 'echo {} $?'.format(finish)
+        self.shell_stdin.write(echo_cmd + '\n')
+        shin = self.shell_stdin
+        self.shell_stdin.flush()
+
+        shout = []
+        sherr = []
+        for line in self.shell_stdout:
+            if str(line).startswith(cmd) or str(line).startswith(echo_cmd):
+                # up for now filled with shell junk from stdin
+                shout = []
+            elif str(line).startswith(finish):
+                # our finish command ends with the exit status
+                exit_status = int(str(line).rsplit(maxsplit=1)[1])
+                if exit_status:
+                    # stderr is combined with stdout.
+                    # thus, swap sherr with shout in a case of failure.
+                    sherr = shout
+                    shout = []
+                break
+            else:
+                # get rid of 'coloring and formatting' special characters
+                shout.append(re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line).
+                             replace('\b', '').replace('\r', ''))
+
+        # first and last lines of shout/sherr contain a prompt
+        if shout and echo_cmd in shout[-1]:
+            shout.pop()
+        if shout and cmd in shout[0]:
+            shout.pop(0)
+        if sherr and echo_cmd in sherr[-1]:
+            sherr.pop()
+        if sherr and cmd in sherr[0]:
+            sherr.pop(0)
+
+        return shin, shout, sherr
+
     def sshExecuteCommand(self, pCommand):
         # 执行命令
         if self.ssh is not None:
-            stdin, stdout, stderr = self.ssh.exec_command(pCommand)
-            stdout.channel.set_combine_stderr(True)
-            stdin.close()
-            consoleOutput = stdout.read().decode('UTF-8')
-            for line in consoleOutput.splitlines():
-                yield line
+            if self.invokeShell:
+                _, stdout, _ = self.sshExecuteShellCommand(pCommand)
+                for line in stdout:
+                    yield str(line.strip('\n'))
+            else:
+                stdin, stdout, stderr = self.ssh.exec_command(pCommand)
+                stdout.channel.set_combine_stderr(True)
+                stdin.close()
+                consoleOutput = stdout.read().decode('UTF-8')
+                for line in consoleOutput.splitlines():
+                    yield line
         else:
             yield "SSH not connected."
 
     def processCommand(self, pSql):
         sql = pSql.strip()
+
+        match_obj = re.match(
+            r"ssh\s+set\s+invokeshell\s+(.*?)$",
+            sql, re.IGNORECASE | re.DOTALL)
+        if match_obj:
+            invoke_shell = match_obj.group(1).strip()
+            if invoke_shell.upper().strip() == "TRUE":
+                self.sshSetInvokeShell(True)
+            else:
+                self.sshSetInvokeShell(False)
 
         match_obj = re.match(
             r"ssh\s+connect\s+(.*?)\s+with\s+user\s+(.*?)\s+key\s+(.*?)$",
